70803c16275dbcbf10ee1e98d0603116303752e3
Hyunkang Son <aoaoaoahah@gmail.com>
2021-08-05 04:39:13 +0900

    '1963'

---
+++ 1963_bfs.py
@@ -0,0 +1,107 @@
+import sys
+from collections import deque
+
+def make(node):
+    x = list(map(int, list(str(node))))
+    y = [[] for _ in range(4)]
+    for i in range(0, 10):
+        y[0].append((i - x[0]) * 1000)
+        y[1].append((i - x[1]) * 100)
+        y[2].append((i - x[2]) * 10)
+        y[3].append(i - x[3])
+    y[0].pop(0)
+
+    merged = []
+    for i in y:
+        for j in i:
+            merged.append(j)
+
+    return merged
+
+
+def eratos_sieve():
+    eratos = [1] * (10001)
+    for i in range(2, 10001):
+        if eratos[i]:
+            for j in range(2 * i, 10001, i):
+                eratos[j] = 0
+
+    return eratos
+
+
+def bfs(st,end):
+    q = deque()
+    q.append((0, st))
+    visit = [0]*10001
+    visit[st] = 1
+
+    while q:
+        dist, node = q.popleft()
+        if node == end:
+            ans.append(dist)
+        merged = make(node)
+        for move in merged:
+            newnode = node + move
+            if 1000 <= newnode <= 9999 and eratos[newnode]:
+                if visit[newnode] == 0:
+                    visit[newnode] = 1
+                    q.append((dist+1,newnode))
+
+
+n = int(sys.stdin.readline())
+nums = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+eratos = eratos_sieve()
+ans = []
+
+for a,b in nums:
+    if a == b:
+        print(0)
+        continue
+    elif a > b:
+        tmp = b
+        b = a
+        a = tmp
+
+    bfs(a,b)
+    if ans:
+        print(ans[-1])
+    else:
+        print("Impossible")
+
+
+"""
+TestCase
+
+input:
+13
+1231 1231
+1231 1237
+1277 9001
+9001 8087
+8263 9391
+9011 8263
+7433 4133
+3797 3467
+8017 1373
+3391 8017
+1109 1019
+9173 1973
+2441 9199
+
+output:
+0
+1
+5
+4
+6
+6
+3
+2
+7
+5
+2
+2
+8
+
+
+"""
---
+++ 1963_dijkstra.py
@@ -0,0 +1,72 @@
+import sys, heapq
+inf = int(1e9)
+
+def make(node):
+    x = list(map(int, list(str(node))))
+    y = [[] for _ in range(4)]
+    for i in range(0, 10):
+        y[0].append((i - x[0]) * 1000)
+        y[1].append((i - x[1]) * 100)
+        y[2].append((i - x[2]) * 10)
+        y[3].append(i - x[3])
+    y[0].pop(0)
+
+    merged = []
+    for i in y:
+        for j in i:
+            merged.append(j)
+
+    return merged
+
+
+def eratos_sieve():
+    eratos = [1] * 10001
+    for i in range(2, 10001):
+        if eratos[i]:
+            for j in range(2 * i, 10001, i):
+                eratos[j] = 0
+
+    return eratos
+
+
+def dijkstra(st, end):
+    queue = []
+    heapq.heappush(queue, (0, st))
+    distance[st] = 0
+
+    while queue:
+        dist, node = heapq.heappop(queue)
+        if distance[node] >= dist:
+            merged = make(node)
+            for move in merged:
+                if 0 <= node + move <= 9999 and eratos[node + move]:
+                    cost = dist + 1
+                    if cost < distance[node + move]:
+                        distance[node + move] = cost
+                        heapq.heappush(queue, (cost, node + move))
+
+
+n = int(sys.stdin.readline())
+nums = []
+nums = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+distance = [inf] * 10001
+
+eratos = eratos_sieve()
+
+for a, b in nums:
+    if a == b:
+        print(0)
+        continue
+    elif a > b:
+        tmp = b
+        b = a
+        a = tmp
+
+    dijkstra(a, b)
+
+    if distance[b] == inf:
+        print("Impossible")
+    else:
+        print(distance[b])
+
+    distance = [inf] * (10001)
