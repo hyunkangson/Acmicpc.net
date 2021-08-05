f3ad1e7064eec64fca7d18b54589b5319e3fe7f2
Hyunkang Son <aoaoaoahah@gmail.com>
2021-08-06 01:59:19 +0900

    'modified'

--- 1987_bfs.py
+++ 1987_bfs.py
@@ -1,5 +1,5 @@
-import sys,copy
-from collections import deque
+import sys
+
 def bfs():
     q = set([(0, 0, graph[0][0])])
     max_len = 1
@@ -7,15 +7,17 @@ def bfs():
     while q:
         tmp = q.pop()
         x, y, visit = tmp[0], tmp[1], tmp[2]
-        for dx,dy in (1,0),(0,1),(-1,0),(0,-1):
-            nx,ny = x+dx,y+dy
+        for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
+            nx, ny = x + dx, y + dy
             if 0 <= nx < c and 0 <= ny < r:
                 if graph[ny][nx] not in visit:
                     tmp = visit + graph[ny][nx]
-                    q.add((nx,ny,tmp))
+                    q.add((nx, ny, tmp))
                     if max_len < len(tmp):
                         max_len = len(tmp)
+
     return max_len
+
 
 r, c = map(int, sys.stdin.readline().split())
 graph = [list(sys.stdin.readline()) for _ in range(r)]
