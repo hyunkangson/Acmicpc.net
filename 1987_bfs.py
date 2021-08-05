b53411ccf27b2562ea6a79529899a48d26933a8c
Hyunkang Son <aoaoaoahah@gmail.com>
2021-08-06 01:44:05 +0900

    'timeout_with_overflow'

--- 1987_bfs.py
+++ 1987_bfs.py
@@ -2,28 +2,22 @@ import sys,copy
 from collections import deque
 def bfs():
     q = deque()
-    q.append([0,0])
-    visit = [[[] for _ in range(c)] for _ in range(r)]
-    visit[0][0].append(graph[0][0])
+    q.append([0, 0, [graph[0][0]]])
+    max_len = 1
+
     while q:
-        x, y = q.popleft()
+        x, y, visit = q.popleft()
         for dx,dy in (1,0),(0,1),(-1,0),(0,-1):
             nx,ny = x+dx,y+dy
             if 0 <= nx < c and 0 <= ny < r:
-                if len(visit[ny][nx]) < len(visit[y][x])+1 and graph[ny][nx] not in visit[y][x]:
-                    tmp = copy.deepcopy(visit[y][x])
+                if graph[ny][nx] not in visit:
+                    tmp = copy.deepcopy(visit)
                     tmp.append(graph[ny][nx])
-                    visit[ny][nx] = tmp
-                    q.append((nx,ny))
-    return visit
+                    q.append([nx,ny,tmp])
+                    if max_len < len(tmp):
+                        max_len = len(tmp)
+    return max_len
 
 r, c = map(int, sys.stdin.readline().split())
 graph = [list(sys.stdin.readline()) for _ in range(r)]
-ans = bfs()
-max_len = 0
-
-for i in ans:
-    for j in i:
-        if len(j) > max_len:
-            max_len = len(j)
-print(max_len)
+print(bfs())
