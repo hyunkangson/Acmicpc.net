a63a839516b3993ba3105c6b1818398430f433d7
Hyunkang Son <aoaoaoahah@gmail.com>
2021-08-06 01:44:44 +0900

    'answer'

--- 1987_bfs.py
+++ 1987_bfs.py
@@ -1,19 +1,18 @@
 import sys,copy
 from collections import deque
 def bfs():
-    q = deque()
-    q.append([0, 0, [graph[0][0]]])
+    q = set([(0, 0, graph[0][0])])
     max_len = 1
 
     while q:
-        x, y, visit = q.popleft()
+        tmp = q.pop()
+        x, y, visit = tmp[0], tmp[1], tmp[2]
         for dx,dy in (1,0),(0,1),(-1,0),(0,-1):
             nx,ny = x+dx,y+dy
             if 0 <= nx < c and 0 <= ny < r:
                 if graph[ny][nx] not in visit:
-                    tmp = copy.deepcopy(visit)
-                    tmp.append(graph[ny][nx])
-                    q.append([nx,ny,tmp])
+                    tmp = visit + graph[ny][nx]
+                    q.add((nx,ny,tmp))
                     if max_len < len(tmp):
                         max_len = len(tmp)
     return max_len
