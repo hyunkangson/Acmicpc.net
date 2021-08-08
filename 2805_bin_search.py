50e0a6454afb342c0c9b550f741f35c093fc1b73
Hyunkang Son <aoaoaoahah@gmail.com>
2021-08-08 23:21:46 +0900

    '2805'

---
+++ 2805_bin_search.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+def search():
+    ed = max(nums)
+
+    while st <= ed:
+        mid = (st+ed)//2
+        summation = 0
+        for n in nums:
+            if n - mid > 0:
+                summation += n - mid
+
+        if summation < m:
+            ed = mid - 1
+        else:
+            st = mid + 1
+
+    return ed
+
+_, m = map(int, stdin.readline().split())
+nums = list(map(int,stdin.readline().split()))
+
+print(search())
