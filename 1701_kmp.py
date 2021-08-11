1bcce479ee730d10aee40c9401311ad91a83a310
Hyunkang Son <aoaoaoahah@gmail.com>
2021-08-12 00:29:08 +0900

    'kmp'

---
+++ 1701_kmp.py
@@ -0,0 +1,21 @@
+import sys
+
+def kmp(string):
+    j = 0
+    table = [0] * len(string)
+    for i in range(1,len(string)):
+        while j > 0 and string[i] != string[j]:
+            j = table[j-1]
+        if string[i] == string[j]:
+            j += 1
+            table[i] = j
+    return max(table)
+
+txt = sys.stdin.readline().rstrip()
+
+ans = 0
+for i in range(len(txt)):
+    pattern = txt[i:len(txt)]
+    ans = max(ans, kmp(pattern))
+
+print(ans)
