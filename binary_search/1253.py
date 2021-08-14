d441f9eed9e6bb73f287d87338c9b6fd67ddfa41
Hyunkang Son <aoaoaoahah@gmail.com>
2021-08-14 17:27:09 +0900

    '1253'

---
+++ binary_search/1253.py
@@ -0,0 +1,31 @@
+import sys
+
+def search(i):
+    global cnt
+    target = nums[i]
+    nums.pop(i)
+    st = 0
+    ed = l - 2
+
+    while st < ed:
+        val = nums[st] + nums[ed]
+        if  val == target:
+            nums.insert(i, target)
+            cnt += 1
+            return
+        elif val < target:
+            ed -=1
+        else:
+            st += 1
+
+    nums.insert(i, target)
+
+
+n = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+nums.sort(reverse=True)
+l = len(nums)
+cnt = 0
+[search(i) for i in range(l)]
+
+print(cnt)
