56f6b07260545b216283d7ee36ca4f73a555b33a
Hyunkang Son <aoaoaoahah@gmail.com>
2021-08-29 20:29:47 +0900

    13913

--- Graph/13913.py
+++ Graph/13913.py
@@ -1,5 +1,5 @@
+from collections import deque
 import sys
-from collections import deque,defaultdict
 
 def bfs():
     q = deque()
@@ -8,20 +8,24 @@ def bfs():
     while q:
         x = q.popleft()
         if x == k:
-            return
+            break
         for y in x-1, x+1, x*2:
-            if 0 <= y <= k and route[y] == 0:
-                route[y] = x
+            if 0 <= y <= 100000 and visit[y] == -1:
                 q.append(y)
+                visit[y] = x
 
 
-n, k = map(int, sys.stdin.readline().split())
-route = defaultdict(int)
+n, k = list(map(int, input().split()))
+visit = [-1] * 100001
+visit[n] = -2
+
 bfs()
-ans = [k]
-while n != k:
-    ans.insert(0, route[k])
-    k = route[k]
 
+ans = []
+while k != -2:
+    ans.append(k)
+    k = visit[k]
+
+ans.reverse()
 print(len(ans)-1)
-print(*ans)
+print(' '.join(map(str, ans)))
