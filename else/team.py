9be6c7f4aeb7de3208daf8dfdad4e6576459719b
Hyunkang Son <aoaoaoahah@gmail.com>
2021-09-01 23:37:07 +0900

    .

---
+++ else/team.py
@@ -0,0 +1,29 @@
+import sys
+
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    if x != y:
+        parent[y] = x
+
+
+def find(x):
+    if parent[x] == x:
+        return x
+    else:
+        parent[x] = find(parent[x])
+        return parent[x]
+
+
+n,m = map(int, sys.stdin.readline().split())
+parent = [int(i) for i in range(n+1)]
+
+for _ in range(m):
+    dec, a, b = map(int, sys.stdin.readline().split())
+    if dec:
+        if find(a) == find(b):
+            print("YES")
+        else:
+            print("NO")
+    else:
+        union(a,b)
