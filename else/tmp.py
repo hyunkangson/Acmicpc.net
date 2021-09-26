f1b9e123131fb967e99d399dd6dc7347e1550f69
Hyunkang Son <aoaoaoahah@gmail.com>
2021-09-27 00:46:16 +0900

    .

---
+++ else/tmp.py
@@ -0,0 +1,21 @@
+import heapq
+
+def solution(n, works):
+    answer = 0
+
+    if sum(works) < n:
+        return answer
+
+    work = [-i for i in works]
+    heapq.heapify(work)
+
+    for _ in range(n):
+        heapq.heappush(work,heapq.heappop(work)+1)
+
+    for w in work:
+        answer += w**2
+
+    return answer
+
+
+print(solution([1,1],	3))
