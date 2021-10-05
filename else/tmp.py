ccd6d6ff390f8f2105946f3ccaa428a4be7b8b74
Hyunkang Son <aoaoaoahah@gmail.com>
2021-10-05 18:09:56 +0900

    .

--- else/tmp.py
+++ else/tmp.py
@@ -1,26 +1,16 @@
-from collections import defaultdict
+from itertools import permutations
 
-def solution(table, languages, preference):
-    pref = defaultdict(int)
+def solution(word):
+    vowel = ["A","E","I","O","U"]*5
+    dic = []
 
-    for i in range(len(languages)):
-        pref[languages[i]] = preference[i]
+    for i in range(1,6):
+        for j in list(permutations(vowel,i)):
+            dic.append("".join(j))
 
-    score = defaultdict(int)
-    for t in table:
-        t = list(map(str, t.split()))
-        for i in range(1,len(t)):
-            if pref[t[i]]:
-                score[t[0]] += pref[t[i]] * (len(t)-i)
+    dic = sorted(list(set(dic)))
 
-    answer = sorted(score.items(), key=lambda x:(-x[1],x[0]))[0][0]
+    return dic.index(word)+1
 
-    return answer
 
-print(solution(["SI JAVA JAVASCRIPT SQL PYTHON C#",
-                "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
-                "HARDWARE C C++ PYTHON JAVA JAVASCRIPT",
-                "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
-                "GAME C++ C# JAVASCRIPT C JAVA"],
-               ["PYTHON", "C++", "SQL"],
-               [7, 5, 5]))
+print(solution("AAAAE"))
