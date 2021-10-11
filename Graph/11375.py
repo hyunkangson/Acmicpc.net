8222446a2f348415891a5088eb4113ed52805658
Hyunkang Son <aoaoaoahah@gmail.com>
2021-10-12 06:12:46 +0900

    Bipartite Matching

--- Graph/11375.py
+++ Graph/11375.py
@@ -8,15 +8,15 @@ def dfs(v):
         visit[w] = 1
         if done[w] == -1 or dfs(done[w]):
             done[w] = v
-            return 1
-    return 0
+            return True
+    return False
 
 
 n,m = map(int, sys.stdin.readline().split())
 works = [list(map(int, sys.stdin.readline().split()))[1:] for _ in range(n)]
-done = [-1] * (m + 1)
 
 cnt = 0
+done = [-1] * (m + 1)
 for i in range(n):
     visit = [0] * (m+1)
     if dfs(i):
