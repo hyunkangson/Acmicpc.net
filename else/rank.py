c7570ed598d91ae7185b9e5ad19102d048cef89a
Hyunkang Son <aoaoaoahah@gmail.com>
2021-11-13 23:45:35 +0900

    .

--- else/rank.py
+++ else/rank.py
@@ -16,7 +16,7 @@ for i in range(1,n+1):
                 dist[j][k] = dist[j][i] + dist[i][k]
 
 ans = 0
-print(dist)
+
 for i in range(1, n + 1):
     cnt = 0
     for j in range(1, n + 1):
@@ -24,7 +24,7 @@ for i in range(1, n + 1):
             cnt += 1
         if dist[j][i] != inf and dist[j][i] != 0:
             cnt += 1
-    if cnt == 5:
+    if cnt == n-1:
         ans += 1
 
 print(ans)
