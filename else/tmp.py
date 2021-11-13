b65e7acf91ad48de2cc569194b0dd40a65c382ff
Hyunkang Son <aoaoaoahah@gmail.com>
2021-11-14 04:04:18 +0900

    .

--- else/tmp.py
+++ else/tmp.py
@@ -1,18 +1,38 @@
 import sys
+from collections import deque
 
-a = sys.stdin.readline().rstrip()
-b = sys.stdin.readline().rstrip()
+for _ in range(int(stdin.readline())):
+    n = int(sys.stdin.readline())
+    nums = list(map(int, sys.stdin.readline().split()))
+    rank = [[0] * (n + 1) for _ in range(n + 1)]
+    in_degree = [0] * (n+1)
 
-graph = [[0]*(len(a)+1) for _ in range(len(b)+1)]
-graph[0] = [int(i) for i in range(len(a)+1)]
-for i in range(len(b)+1):
-    graph[i][0] = i
+    for i in range(n):
+        for j in range(i+1, n):
+            rank[nums[i]][nums[j]] = 1
 
-for j in range(1,len(a)+1):
-    for i in range(1,len(b)+1):
-        if b[i-1] == a[j-1]:
-            graph[i][j] = graph[i-1][j-1]
-        else:
-            graph[i][j] = min(graph[i-1][j],graph[i][j-1],graph[i-1][j-1]) + 1
+    m = int(stdin.readline())
+    for _ in range(m):
+        a, b = map(int, sys.stdin.readline().split())
+        rank[a][b], rank[b][a] = rank[b][a], rank[a][b]
 
-print(graph[-1][-1])
+    ans = []
+    q = deque()
+    for i in range(1, n+1):
+        in_degree[i] = rank[i].count(1)
+        if in_degree[i] == 0:
+            q.append(i)
+
+    while q:
+        cur = q.popleft()
+        ans.append(cur)
+        for i in range(1, n+1):
+            if rank[i][cur]:
+                in_degree[i] -= 1
+                if in_degree[i] == 0:
+                    q.append(i)
+
+    if len(ans) == n:
+        print(*ans[::-1])
+    else:
+        print("IMPOSSIBLE")
