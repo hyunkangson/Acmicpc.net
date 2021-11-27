cf8b8824189e2cd82f811d15e481d85653469cdd
Hyunkang Son <aoaoaoahah@gmail.com>
2021-11-27 21:02:02 +0900

    .

---
+++ else/설탕배달.py
@@ -0,0 +1,15 @@
+import sys
+
+
+n = int(sys.stdin.readline())
+cnt = 0
+
+while True:
+    if n%5 == 0:
+        print(n//5+cnt)
+        break
+    elif n <= 0:
+        print(-1)
+        break
+    n -= 3
+    cnt += 1
